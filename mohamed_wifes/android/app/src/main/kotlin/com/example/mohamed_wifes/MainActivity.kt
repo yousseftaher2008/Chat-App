fd7c89cf7a537f5e13722a4a0c068eacb3c0b23c
yhamrosh <yhamrosh@gmail.com>
2023-08-04 19:43:31 +0300

    remove the un used projects

--- mohamed_wifes/android/app/src/main/kotlin/com/example/mohamed_wifes/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mohamed_wifes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- redux_course/android/app/src/main/kotlin/com/example/redux/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.redux_course
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
